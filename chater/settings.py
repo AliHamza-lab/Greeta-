4803ebd32c963a15c8bb1040d3f7fd4551610969
Ali hamza shabbir <160996641+soulspy12@users.noreply.github.com>
2024-06-15 13:06:28 -0700

    Update settings.py

--- chater/settings.py
+++ chater/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['walrus-app-ahyy3.ondigitalocean.app', 'your-other-allowed-hosts', ...]
 # Application definition
 
 INSTALLED_APPS = [
