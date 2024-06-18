09922dff4a9baf5879b8914131ebf0db357f598b
Ali hamza shabbir <160996641+soulspy12@users.noreply.github.com>
2024-06-18 14:53:15 -0700

    Delete chater/context_processors.py

--- chater/context_processors.py
+++
@@ -1,9 +0,0 @@
-# chater/context_processors.py
-
-from django.conf import settings
-
-def extra_context(request):
-    return {
-        'SCRIPT_URL': settings.SCRIPT_URL,
-        'CONFIG_SCRIPT_URL': settings.CONFIG_SCRIPT_URL,
-    }
